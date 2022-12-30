290a283133f20ffe9c8437055282e310965eb1ae
Slepy <92794194+Slepy1@users.noreply.github.com>
2022-12-30 15:36:35 +0000

    Update main03.py

--- main03.py
+++ main03.py
@@ -2,7 +2,7 @@
 #from dhl11 sensor to the aws iot module using mqtt
 
 #version 0.3
-#this version uses gpio library to listen for button presses and threads
+#this version uses gpio library to listen for button presses and callbacks to save some resources
 
 #todo
 #the code is working
@@ -19,31 +19,31 @@ import Adafruit_DHT
 def setup():
     GPIO.setmode(GPIO.BOARD) #use physical pin numbers
     GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #setup button
-    GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW) #setup led
+    GPIO.add_event_detect(37, GPIO.RISING, callback=main, bouncetime=200) #add event, this listens for button presses
 
-def motorcontrol():
-    while True:
-        sleep(0.1)
-        button=GPIO.input(37)
-        if button==1: #Robot is activated when button is pressed
-            print ("start ")
+def main(channel):
 
-            GPIO.output(16, GPIO.HIGH) # Turn on
+    os.system("sudo insmod piio.ko") #turn LED on
+    humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.DHT11, 17) #take the reading form the sensor
+    if humidity is not None and temperature is not None: #check if its not empty
+        #send the sensor reading to aws using MQTT_publish.py
+        os.system("python3 MQTT_publish.py --endpoint a5ta3504qdjkh-ats.iot.eu-central-1.amazonaws.com --ca_file ~/certs/root-CA.crt --cert ~/certs/raspberry_pi_test.cert.pem --key ~/certs/raspberry_pi_test.private.key --client_id basicPubSub --sensor_reading {0:0.1f}".format(temperature))
+    else:#reading failed, do not update
+        print("Sensor Error");
+    os.system("sudo rmmod piio.ko") #turn the LED off
 
-            humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.DHT11, 17) #take the reading form the sensor
-            if humidity is not None and temperature is not None: #check if its not empty
-                #send the sensor reading to aws using MQTT_publish.py
-                os.system("python3 MQTT_publish.py --endpoint a5ta3504qdjkh-ats.iot.eu-central-1.amazonaws.com --ca_file ~/certs/root-CA.crt --cert ~/certs/raspberry_pi_test.cert.pem --key ~/certs/raspberry_pi_test.private.key --client_id basicPubSub --sensor_reading {0:0.1f}".format(temperature))
-            else:#reading failed, do not update
-                print("Sensor Error");
-            GPIO.output(16, GPIO.LOW)  # Turn off
+def destroy(): #clean up
+    GPIO.cleanup() 
+    os.system("sudo rmmod piio.ko")         
 
-def destroy():
-    GPIO.cleanup()                     # Release resource               
-
-if __name__ == '__main__':     # Program start from here
+if __name__ == '__main__':
     setup()
     try:
-        motorcontrol()
-    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        while 1:
+            sleep(30)
+
+    except KeyboardInterrupt:
+        print("Program closed by user")
+
+    finally:
         destroy()
