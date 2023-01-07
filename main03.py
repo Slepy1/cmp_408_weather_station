8c29950a12fa47923bc2636ddd81f12a22520abc
Slepy <92794194+Slepy1@users.noreply.github.com>
2023-01-07 23:40:26 +0000

    final ver
    replaced piio.c with other driver xdxd

--- main03.py
+++ main03.py
@@ -4,9 +4,13 @@
 #version 0.3
 #this version uses gpio library to listen for button presses and callbacks to save some resources
 
+#version 1
+#replaced the piio.c file, now rather than controlling led with command os.system("sudo insmod piio.ko")
+#the file /dev/piiodev is opened and led is turned on by os.write(dev,data) and turned off by os.read(dev, test)
+
 #todo
 #the code is working
-#if have time try to replace gpio library with your own lkm 
+#if have time try improving the piio.c
 
 #notes for future debugging
 #1. connect button to 3.3v not to gnd. 2.dont use that oled screen, its broken. 3. 10k omh resistor is too high.
@@ -16,25 +20,28 @@ from time import sleep
 import os
 import Adafruit_DHT
 
+data = bytes("hello", 'ascii')
+dev = os.open("/dev/piiodev", os.O_RDWR)
+test = 1
+
 def setup():
     GPIO.setmode(GPIO.BOARD) #use physical pin numbers
     GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #setup button
     GPIO.add_event_detect(37, GPIO.RISING, callback=main, bouncetime=200) #add event, this listens for button presses
 
 def main(channel):
-
-    os.system("sudo insmod piio.ko") #turn LED on
+    os.write(dev,data) #turn LED on
     humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.DHT11, 17) #take the reading form the sensor
     if humidity is not None and temperature is not None: #check if its not empty
         #send the sensor reading to aws using MQTT_publish.py
         os.system("python3 MQTT_publish.py --endpoint a5ta3504qdjkh-ats.iot.eu-central-1.amazonaws.com --ca_file ~/certs/root-CA.crt --cert ~/certs/raspberry_pi_test.cert.pem --key ~/certs/raspberry_pi_test.private.key --client_id basicPubSub --sensor_reading {0:0.1f}".format(temperature))
     else:#reading failed, do not update
         print("Sensor Error");
-    os.system("sudo rmmod piio.ko") #turn the LED off
+    os.read(dev, test) #turn the LED off
 
 def destroy(): #clean up
     GPIO.cleanup() 
-    os.system("sudo rmmod piio.ko")         
+    os.read(dev, test) #turn the LED off        
 
 if __name__ == '__main__':
     setup()
